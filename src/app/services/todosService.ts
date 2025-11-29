cd3cc1a871f105f829fe54605a70560e45669bfd
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-11-29 02:13:46 -0500

    Make API call to get mock todo data from an external API and render it

--- src/app/services/todosService.ts
+++ src/app/services/todosService.ts
@@ -1,20 +1,13 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { Todo } from '../model/todo.type';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class TodosService {
-  todoItems: Array<Todo> = [{
-    title:"groceries",
-    id:0,
-    userId:1,
-    completed:false
-  },
-{
-    title:"car wash",
-    id:2,
-    userId:1,
-    completed:false
-  }]
+  http = inject(HttpClient)
+  getTodos = () =>{
+    return this.http.get<Array<Todo>>("https://jsonplaceholder.typicode.com/todos");
+  }
 }
--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -1,17 +1,27 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { TodosService } from '../services/todosService';
 import { Todo } from '../model/todo.type';
+import { catchError } from 'rxjs';
 @Component({
   selector: 'app-todos',
   imports: [],
   templateUrl: './todos.html',
   styleUrl: './todos.css',
 })
-export class Todos implements OnInit{
-  todosService = inject(TodosService)
-  todoItems = signal<Array<Todo>>([])
+export class Todos implements OnInit {
+  todosService = inject(TodosService);
+  todoItems = signal<Array<Todo>>([]);
   ngOnInit(): void {
-      console.log(this.todosService.todoItems)
-      this.todoItems.set(this.todosService.todoItems)
+    this.todosService
+      .getTodos()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          throw err;
+        })
+      )
+      .subscribe((todos)=>{
+        this.todoItems.set(todos);
+      });
   }
 }

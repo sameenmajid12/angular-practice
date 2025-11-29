3ef6615687a23cb4ceace9c572168a6ec634fcb1
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-11-29 00:33:35 -0500

    Move counter component to components folder

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { Greeter } from '../components/greeter/greeter';
-import { Counter } from '../counter/counter';
+import { Counter } from '../components/counter/counter';
 
 @Component({
   selector: 'app-home',

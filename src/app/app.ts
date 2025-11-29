17be34249e4a9519abc11aec116f1359a459434f
Saminraiyan12 <samin.raiyan1@gmail.com>
2025-11-28 23:35:51 -0500

    Create root component that renders app-header and app-home

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './home/home';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, Home, Header],
+  template: `
+    <app-header></app-header>
+    <app-home></app-home> `,
+  styles: [
+    `
+      h1 {
+        color: red;
+      }
+    `,
+  ],
 })
 export class App {
-  protected readonly title = signal('e-commerce-app');
+  protected readonly title = signal('angular-practice');
 }
